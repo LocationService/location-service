ce8d0a9407c525d5e34f42e2836f121bb4ab6d3b
Andrew Ostroumov <andrew.ostroumov@gmail.com>
2017-11-08 21:29:34 +0200

    Request permissions when activity started

--- app/src/main/java/com/c137/location/MainActivity.java
+++ app/src/main/java/com/c137/location/MainActivity.java
@@ -1,23 +1,71 @@
 package com.c137.location;
 
-import android.os.Bundle;
+import android.Manifest;
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.content.Intent;
-import android.view.View;
+import android.support.v4.app.ActivityCompat;
 
 public class MainActivity extends Activity {
+    private String[] permissions = new String[]{
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.READ_PHONE_STATE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestPermissions();
     }
 
-    public void onClickStart(View v) {
-        startService(new Intent(this, LocationService.class));
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (grantAllPermissions(grantResults)) {
+            disableActivity();
+            startLocationService();
+            finish();
+        } else {
+            requestPermissions();
+        }
+
     }
 
-    public void onClickStop(View v) {
-        stopService(new Intent(this, LocationService.class));
+    private void disableActivity() {
+        Context context = getApplicationContext();
+        String packageName = context.getPackageName();
+        ComponentName component = new ComponentName(packageName, packageName + ".MainActivity");
+
+        context.getPackageManager().setComponentEnabledSetting(
+                component,
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP
+        );
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, permissions, 1);
+    }
+
+    private void startLocationService() {
+        Context context = getApplicationContext();
+        Intent serviceIntent = new Intent(context, LocationService.class);
+        context.startService(serviceIntent);
+    }
+
+    private boolean grantAllPermissions(int[] grantResults) {
+        if (grantResults.length != permissions.length) {
+            return false;
+        }
+
+        for (int i = 0; i < permissions.length; i++) {
+            if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
     }
 }
